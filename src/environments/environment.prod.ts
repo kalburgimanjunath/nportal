666d6042b8d60170531ac43391c65473e84f6201
manjunath <manjunathkalburgi65512@gmail.com>
2019-11-23 21:39:38 +0530

    'add'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,10 @@
 export const environment = {
   production: true,
   youtube: {
-    API_KEY: 'AIzaSyA6DqzRUQNRfMIpl8RZjrMWPdh8MOPJfpI',
-    CLIENT_ID: '153683213810-pd0u20b1nj2vtvnerfo1l8vh06uqi4m6.apps.googleusercontent.com'
+    // API_KEY: 'AIzaSyA6DqzRUQNRfMIpl8RZjrMWPdh8MOPJfpI',
+    // CLIENT_ID: '153683213810-pd0u20b1nj2vtvnerfo1l8vh06uqi4m6.apps.googleusercontent.com'
+
+    API_KEY: 'AIzaSyDLajcaXx7ULbaZVStMhQzo7MM8u34QMYI',
+    CLIENT_ID:'153683213810-pd0u20b1nj2vtvnerfo1l8vh06uqi4m6.apps.googleusercontent.com'
   }
 };
