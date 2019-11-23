eef97a2362ee91e26a12cc7e9dec6d5e47124a9c
manjunath <manjunathkalburgi65512@gmail.com>
2019-11-23 21:55:48 +0530

    'add'

--- src/environments/environment.integration.ts
+++ src/environments/environment.integration.ts
@@ -5,10 +5,10 @@
 export const environment = {
   production: true,
   youtube: {
-    API_KEY: 'AIzaSyDCGg6FG6s_zxACqel09vQUKBc-x26pKFA',
-    CLIENT_ID: '971861197531-hm7solf3slsdjc4omsfti4jbcbe625hs'
-    // API_KEY: 'AIzaSyDLajcaXx7ULbaZVStMhQzo7MM8u34QMYI',
-    // CLIENT_ID:'153683213810-pd0u20b1nj2vtvnerfo1l8vh06uqi4m6.apps.googleusercontent.com'
+    // API_KEY: 'AIzaSyDCGg6FG6s_zxACqel09vQUKBc-x26pKFA',
+    // CLIENT_ID: '971861197531-hm7solf3slsdjc4omsfti4jbcbe625hs'
+    API_KEY: 'AIzaSyDLajcaXx7ULbaZVStMhQzo7MM8u34QMYI',
+    CLIENT_ID:'153683213810-pd0u20b1nj2vtvnerfo1l8vh06uqi4m6.apps.googleusercontent.com'
 
   }
 };
