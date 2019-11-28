d0b7dd0e7d139ed6cd157e92f506bf16157d941f
manjunath <manjunathkalburgi65512@gmail.com>
2019-11-28 05:53:24 +0530

    'add'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,15 @@ export const environment = {
     // API_KEY: 'AIzaSyA6DqzRUQNRfMIpl8RZjrMWPdh8MOPJfpI',
     // CLIENT_ID: '153683213810-pd0u20b1nj2vtvnerfo1l8vh06uqi4m6.apps.googleusercontent.com'
 
-    API_KEY: 'AIzaSyDLajcaXx7ULbaZVStMhQzo7MM8u34QMYI',
-    CLIENT_ID: '153683213810-pd0u20b1nj2vtvnerfo1l8vh06uqi4m6.apps.googleusercontent.com'
+    // working
+    API_KEY: 'AIzaSyDCGg6FG6s_zxACqel09vQUKBc-x26pKFA',
+    CLIENT_ID: '971861197531-hm7solf3slsdjc4omsfti4jbcbe625hs'
+
+    // API_KEY: 'AIzaSyBewnzKgqtSZAzigw-gPmGbIyYD0iyQyZc',
+    // CLIENT_ID: '153683213810-pd0u20b1nj2vtvnerfo1l8vh06uqi4m6.apps.googleusercontent.com'
+
+    // API_KEY: 'AIzaSyBewnzKgqtSZAzigw-gPmGbIyYD0iyQyZc',
+    // CLIENT_ID: '206308346493-7gdgqdk41g8vlnqe4913cck8cses38t3.apps.googleusercontent.com'
+
   }
 };
